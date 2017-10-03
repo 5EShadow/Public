4c907c5476524e5ee0c9990d7fe0ba52a9cb4967
D4NP0UL1N <ct3d.f1l35@gmail.com>
2017-10-03 12:58:25 -0400

    Update start.ps1

--- TTW/start.ps1
+++ TTW/start.ps1
@@ -3,10 +3,10 @@ $ErrorActionPreference = 'SilentlyContinue'
 Remove-Item C:\windows\system32\psexec.exe -force
 start-sleep -s 1
 
-Remove-Item C:\windows\system32\setup1.ps1 -force
+Remove-Item C:\Users\administrator\appdata\local\temp\setup1.ps1 -force
 start-sleep -s 1
 
-Remove-Item C:\windows\system32\setup2.ps1 -force
+Remove-Item C:\Users\administrator\appdata\local\temp\setup2.ps1 -force
 start-sleep -s 3
 
 Write-Output "The password for the next level is the Powershell build version. Please be sure to include all periods" -n > C:\Users\Apprentice01\Desktop\challenge.txt
