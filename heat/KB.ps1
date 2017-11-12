b2a3b27c1ce0103d12670d4a28cc844de54e034a
D4NP0UL1N <ct3d.f1l35@gmail.com>
2017-11-12 17:26:41 -0500

    Delete KB.ps1

--- heat/KB.ps1
+++
@@ -1,6 +0,0 @@
-$KBS = @("4025341","4014504")
-foreach ($KB in $KBS) {
-	if ( get-hotfix KB$KB -ErrorAction SilentlyContinue ) {
-		Start-Process wusa.exe -ArgumentList "/KB:$KB /uninstall /quiet /norestart" -Wait
-	}
-}
